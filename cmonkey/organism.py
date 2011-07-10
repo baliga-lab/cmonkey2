5d6ec2bf352ec788cecdde1dfd17e5a947aadd8a
Wei-ju Wu <Weiju.Wu@gmail.com>
2011-07-10 16:44:53 -0700

    organism construction is now formalized in OrganismFactory

--- cmonkey/organism.py
+++ cmonkey/organism.py
@@ -25,12 +25,38 @@ def get_go_taxonomy_id(go_taxonomy_file, organism_name):
     return None
 
 
+class OrganismFactory:
+    """Factory to create an organism. Construction of an organism
+    instance is relatively complex and costly, so it is coordinated
+    here. Information has to be pulled together from various databases
+    which are provided to the factory as configuration parameters.
+    """
+
+    def __init__(self, kegg_organism_mapper,
+                 rsat_organism_mapper,
+                 go_taxonomy_mapper):
+        self.kegg_organism_mapper = kegg_organism_mapper
+        self.rsat_organism_mapper = rsat_organism_mapper
+        self.go_taxonomy_mapper = go_taxonomy_mapper
+
+    def create(self, organism_code):
+        """factory method to create an organism from a code"""
+        kegg_organism = self.kegg_organism_mapper.get_organism(organism_code)
+        rsat_organism = self.rsat_organism_mapper.get_organism(kegg_organism)
+        go_taxonomy_id = self.go_taxonomy_mapper.get_taxonomy_id(rsat_organism)
+        return Organism(organism_code, kegg_organism, rsat_organism,
+                        go_taxonomy_id)
+
+
 class Organism:
     """Abstraction of an organism in cMonkey. It captures all organism-specific
     aspects"""
-    def __init__(self, code):
+    def __init__(self, code, kegg_organism, rsat_organism, go_taxonomy_id):
         """create an Organism instance"""
         self.code = code
+        self.kegg_organism = kegg_organism
+        self.rsat_organism = rsat_organism
+        self.go_taxonomy_id = go_taxonomy_id
 
     def is_prokaryote(self):
         """determine whether this organism is a prokaryote"""
@@ -40,10 +66,6 @@ class Organism:
         """determine whether this organism is a eukaryote"""
         return not self.is_prokaryote()
 
-    @classmethod
-    def create(cls, organism_code):
-        """factory method to create an organism from a code"""
-        return Organism(organism_code)
 
-
-__all__ = ['get_organism_for_code', 'get_go_taxonomy_id', 'Organism']
+__all__ = ['get_organism_for_code', 'get_go_taxonomy_id', 'Organism',
+           'OrganismFactory']
--- test/organism_test.py
+++ test/organism_test.py
@@ -5,7 +5,8 @@ more information and licensing details.
 """
 import unittest
 from util import DelimitedFile
-from organism import get_kegg_organism_for_code, get_go_taxonomy_id, Organism
+from organism import get_kegg_organism_for_code, get_go_taxonomy_id
+from organism import OrganismFactory
 
 TAXONOMY_FILE_PATH = "testdata/KEGG_taxonomy"
 PROT2TAXID_FILE_PATH = "testdata/proteome2taxid"
@@ -47,12 +48,42 @@ class GoTaxonomyMappingTest(unittest.TestCase):  # pylint: disable-msg=R0904
         self.assertIsNone(get_go_taxonomy_id(dfile, 'does not exist'))
 
 
+class MockKeggOrganismMapper:
+    """mock KEGG organism mapper"""
+
+    def get_organism(self, _):
+        """returns an organism for the given code"""
+        return "KEGG organism"
+
+
+class MockRsatOrganismMapper:
+    """mock RSAT organism mapper"""
+
+    def get_organism(self, _):
+        """returns an organism for a KEGG organism"""
+        return "RSAT organism"
+
+
+class MockGoTaxonomyMapper:
+    """mock GO taxonomy mapper"""
+
+    def get_taxonomy_id(self, _):
+        """returns a taxonomy id for an RSAT organism"""
+        return "GO taxonomy id"
+
+
 class OrganismTest(unittest.TestCase):  # pylint: disable-msg=R0904
     """Test class for Organism"""
 
     def test_create(self):
         """tests the create function"""
-        organism = Organism.create('hpy')
+        factory = OrganismFactory(MockKeggOrganismMapper(),
+                                  MockRsatOrganismMapper(),
+                                  MockGoTaxonomyMapper())
+        organism = factory.create('hpy')
         self.assertEquals('hpy', organism.code)
+        self.assertEquals('KEGG organism', organism.kegg_organism)
+        self.assertEquals('RSAT organism', organism.rsat_organism)
+        self.assertEquals('GO taxonomy id', organism.go_taxonomy_id)
         self.assertTrue(organism.is_prokaryote())
         self.assertFalse(organism.is_eukaryote())
