e4019dde40f098a0319e0ba48123899ff654f23f
Wei-ju Wu <Weiju.Wu@gmail.com>
2011-09-22 11:12:29 -0700

    added missing files - oops

---
+++ cmonkey/membership.py
@@ -0,0 +1,98 @@
+"""membership.py - cMonkey cluster membership functionality
+This module captures the microarray-specific scoring component
+of cMonkey.
+
+This file is part of cMonkey Python. Please see README and LICENSE for
+more information and licensing details.
+"""
+
+
+class ClusterMembership:
+    """A class to store row and column memberships of an input matrix
+    1. Row memberships are stored as a matrix where each row represents a gene
+    in the input and num_clusters_per_row columns.
+    2. Column memberships are stored as a matrix of |input genes| rows and
+       num_clusters_per_column columns columns
+    When creating a ClusterMembership, a row membership seed function is
+    called to generate the first column of cluster memberships
+    This function is of the type
+    DataMatrix => [int]
+    A column seed function is called after this, which generates the
+    entire column membership matrix.
+    """
+    def __init__(self, num_clusters, row_is_member_of, column_is_member_of):
+        """creates an instance of ClusterMembership"""
+
+        def create_cluster_to_names_map(name_to_cluster_map):
+            """from a name->cluster-list dictionary, create a cluster->name
+            list dictionary"""
+            result = {}
+            for name, clusters in name_to_cluster_map.items():
+                for cluster in clusters:
+                    if cluster not in result:
+                        result[cluster] = []
+                    result[cluster].append(name)
+            return result
+
+        self.__row_is_member_of = row_is_member_of
+        self.__column_is_member_of = column_is_member_of
+        self.__cluster_row_members = create_cluster_to_names_map(
+            self.__row_is_member_of)
+        self.__cluster_column_members = create_cluster_to_names_map(
+            self.__column_is_member_of)
+
+    # pylint: disable-msg=R0913
+    @classmethod
+    def create(cls, data_matrix,
+               num_clusters,
+               num_clusters_per_row,
+               num_clusters_per_column,
+               seed_row_memberships,
+               seed_column_memberships):
+        """create instance of ClusterMembership using
+        the provided seeding algorithms"""
+        def make_member_map(membership, names):
+            """using a membership array, build a dictionary representing
+            the contained memberships for a name"""
+            result = {}
+            for row_index in range(len(names)):
+                row = membership[row_index]
+                result[names[row_index]] = sorted([row[col_index] for col_index
+                                                   in range(len(row))
+                                                   if row[col_index] > 0])
+            return result
+
+        # using the seeding functions, build the initial membership
+        # dictionaries
+        num_rows = data_matrix.num_rows()
+        row_membership = [[0 for _ in range(num_clusters_per_row)]
+                          for _ in range(num_rows)]
+        seed_row_memberships(row_membership, data_matrix)
+        column_membership = seed_column_memberships(
+            data_matrix, row_membership, num_clusters, num_clusters_per_column)
+        row_is_member_of = make_member_map(row_membership,
+                                           data_matrix.row_names())
+        col_is_member_of = make_member_map(column_membership,
+                                           data_matrix.column_names())
+        return ClusterMembership(num_clusters, row_is_member_of,
+                                 col_is_member_of)
+
+    def clusters_for_row(self, row_name):
+        """determine the clusters for the specified row"""
+        return self.__row_is_member_of[row_name]
+
+    def clusters_for_column(self, column_name):
+        """determine the clusters for the specified column"""
+        return self.__column_is_member_of[column_name]
+
+    def rows_for_cluster(self, cluster):
+        """determine the rows that belong to a cluster"""
+        return self.__cluster_row_members[cluster]
+
+    def columns_for_cluster(self, cluster):
+        """determine the rows that belong to a cluster"""
+        return self.__cluster_column_members[cluster]
+
+    def is_row_member_of(self, row_name, cluster):
+        """determines whether a certain row is member of a cluster"""
+        return row_name in self.rows_for_cluster(cluster)
---
+++ test/membership_test.py
@@ -0,0 +1,122 @@
+"""microarray_test.py - unit test module for microarray module
+
+This file is part of cMonkey Python. Please see README and LICENSE for
+more information and licensing details.
+"""
+import unittest
+import membership as memb
+import datamatrix as dm
+import microarray as ma
+
+
+class MockSeedRowMemberships:
+
+    def __init__(self):
+        self.was_called = False
+
+    def __call__(self, row_membership, data_matrix):
+        self.was_called = True
+
+
+class MockSeedColumnMemberships:
+
+    def __init__(self):
+        self.was_called = False
+
+    def __call__(self, data_matrix, row_membership, num_clusters,
+                 num_clusters_per_column):
+        self.was_called = True
+        return [[0], [0], [0], [0], [0]]
+
+MATRIX1 = [[-0.8682966, 0.0, -0.34731863, 1.786210, 0.0],
+           [-0.7642219, 0.008938267, -0.33071589, 1.850221, 0.0],
+           [-0.5507068, 0.045892230, -0.22028270, 1.991723, 0.0],
+           [-0.3798518, 0.061836346, -0.24734538, 2.058267, 0.0],
+           [-0.5656293, 0.087864752, -0.09335630, 2.020889, 0.0],
+           [-0.6259898, 0.071135199, -0.14227040, 1.977559, 0.0],
+           [-0.6856569, 0.0, -0.23374668, 1.924514, 0.07791556],
+           [-0.5205586, 0.0, -0.09943255, 2.041292, 0.08773460],
+           [-0.8318400, 0.175124204, -0.06254436, 1.882585, 0.0],
+           [-0.6615701, 0.0, -0.06343823, 1.975648, 0.07250084]]
+
+
+class MockDataMatrix:
+
+    def __init__(self, num_rows):
+        self.__num_rows = num_rows
+
+    def num_rows(self):
+        return self.__num_rows
+
+    def num_columns(self):
+        return 5
+
+    def row_names(self):
+        return [('GENE%d' % index) for index in range(self.__num_rows)]
+
+    def column_names(self):
+        return [('COND%d' % index) for index in range(self.num_columns())]
+
+    def values(self):
+        return MATRIX1
+
+    def submatrix(self, rows, cols):
+        return self
+
+
+class ClusterMembershipTest(unittest.TestCase):
+    """Test class for ClusterMembership"""
+
+    def test_constructor(self):
+        """tests the constructor"""
+        membership = memb.ClusterMembership(3,
+                                            {'R1': [1, 3], 'R2': [2, 3]},
+                                            {'C1': [1, 2], 'C2': [2]})
+        self.assertEquals([1, 3], membership.clusters_for_row('R1'))
+        self.assertEquals([2, 3], membership.clusters_for_row('R2'))
+        self.assertEquals([1, 2], membership.clusters_for_column('C1'))
+        self.assertEquals([2], membership.clusters_for_column('C2'))
+        self.assertEquals(['R1'], membership.rows_for_cluster(1))
+        self.assertEquals(['R2'], membership.rows_for_cluster(2))
+        self.assertEquals(['R1', 'R2'], membership.rows_for_cluster(3))
+        self.assertEquals(['C1'], membership.columns_for_cluster(1))
+        self.assertTrue(membership.is_row_member_of('R1', 1))
+        self.assertTrue(membership.is_row_member_of('R2', 2))
+        self.assertFalse(membership.is_row_member_of('R1', 2))
+
+    def test_create_membership(self):
+        """test creating a ClusterMembership object"""
+        datamatrix = MockDataMatrix(3)
+        seed_row_memberships = MockSeedRowMemberships()
+        seed_col_memberships = MockSeedColumnMemberships()
+        membership = memb.ClusterMembership.create(datamatrix, 1, 2, 2,
+                                                   seed_row_memberships,
+                                                   seed_col_memberships)
+        self.assertTrue(seed_row_memberships.was_called)
+        self.assertTrue(seed_col_memberships.was_called)
+
+    def test_compute_column_scores_submatrix(self):
+        """tests compute_column_scores_submatrix"""
+        matrix = dm.DataMatrix(10, 5, ['R1', 'R2', 'R3', 'R4', 'R5', 'R6',
+                                       'R7', 'R8', 'R9', 'R10'],
+                               ['C1', 'C2', 'C3', 'C4', 'C5'],
+                               MATRIX1)
+        result = ma.compute_column_scores_submatrix(matrix)
+        scores = result[0]
+        self.assertEqual(5, len(scores))
+        self.assertAlmostEqual(0.03085775, scores[0])
+        self.assertAlmostEqual(0.05290099, scores[1])
+        self.assertAlmostEqual(0.05277032, scores[2])
+        self.assertAlmostEqual(0.00358045, scores[3])
+        self.assertAlmostEqual(0.03948821, scores[4])
+
+    def test_seed_column_members(self):
+        """tests seed_column_members"""
+        data_matrix = dm.DataMatrix(3, 2, ["GENE1", "GENE2", "GENE3"],
+                                    ['COL1', 'COL2'], [[1.0, 2.0], [2.0, 1.0],
+                                                    [2.0, 1.0]])
+        row_membership = [[1, 0], [2, 0], [1, 0]]
+        column_members = ma.seed_column_members(data_matrix, row_membership,
+                                                2, 2)
+        self.assertEquals([2, 1], column_members[0])
+        self.assertEquals([2, 1], column_members[1])
