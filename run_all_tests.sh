841f996f1fee8482bedd59f301e5dd3cf1000f27
Wei-ju Wu <weiju.wu@gmail.com>
2012-06-08 14:26:05 -0700

    added script to run all tests contained in the test suite

---
+++ run_all_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running unit tests..."
+PYTHONPATH=`pwd`/cmonkey python test/all_tests.py
+echo "Running integration tests..."
+PYTHONPATH=`pwd`/cmonkey python test/all_integration_tests.py
+echo "Running iteration test..."
+PYTHONPATH=`pwd`/cmonkey python test/iteration_test.py
+echo "Running postprocessing test..."
+PYTHONPATH=`pwd`/cmonkey python test/postproc_test.py
