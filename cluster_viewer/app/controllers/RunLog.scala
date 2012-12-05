b80345eeb57bdb1b3bfe4e78bab68f647515baab
Wei-ju Wu <weiju.wu@gmail.com>
2012-12-05 13:11:44 -0800

    run log is now read from SQLite database

--- cluster_viewer/app/controllers/RunLog.scala
+++ cluster_viewer/app/controllers/RunLog.scala
@@ -1,9 +1,54 @@
 package controllers
 
-import play.api.libs.json._
+import play.api.Play.current
 import java.io._
 import java.util.regex._
 import scala.collection.mutable.HashMap
+
+import play.api.db._
+import org.scalaquery.ql._
+import org.scalaquery.ql.TypeMapper._
+import org.scalaquery.ql.extended.{ExtendedTable => Table}
+import org.scalaquery.ql.extended.SQLiteDriver.Implicit._
+import org.scalaquery.session.{Database, Session}
+
+object RunLogs extends Table[(Long, String)]("run_logs") {
+  lazy val database = Database.forDataSource(DB.getDataSource())
+  def id = column[Long]("rowid", O PrimaryKey,  O AutoInc, O NotNull)
+  def name = column[String]("name", O NotNull)
+  def * = id ~ name
+  def findAll = database.withSession { implicit db: Session =>
+    (for {
+      t <- this
+      _ <- Query orderBy(t.name)
+     } yield t.id ~ t.name).list
+  }
+}
+
+object LogEntries extends Table[(Long, Long, Long, Boolean, Float)]("log_entries") {
+  lazy val database = Database.forDataSource(DB.getDataSource())
+  def id = column[Long]("rowid", O PrimaryKey,  O AutoInc, O NotNull)
+  def logId = column[Long]("log_id", O NotNull)
+  def iteration = column[Long]("iteration", O NotNull)
+  def wasActive = column[Boolean]("was_active", O NotNull)
+  def scaling = column[Float]("scaling", O NotNull)
+
+  def * = id ~ logId ~ iteration ~ wasActive ~ scaling
+
+  def findAll = database.withSession { implicit db: Session =>
+    (for {
+      t <- this
+      _ <- Query orderBy(t.iteration)
+     } yield t.id ~ t.logId ~ t.iteration ~ t.wasActive ~ t.scaling).list
+  }
+
+  def entriesFor(runLogId: Long) = database.withSession { implicit db: Session =>
+    (for {
+      logEntry <- LogEntries if logEntry.logId === runLogId
+      _ <- Query.orderBy(logEntry.iteration)
+    } yield logEntry.wasActive ~ logEntry.scaling).list
+  }
+}
 
 case class RunLog(functionName: String, scaling: Array[Float],
                   active: Array[Boolean]) {
@@ -12,63 +57,14 @@ case class RunLog(functionName: String, scaling: Array[Float],
   }
 }
 
-object RunLogReader {
-  val FilePattern = Pattern.compile("(\\d+)-runlog.json")
-}
-
 class RunLogReader {
-  import RunLogReader._
-
-  implicit object RunLogFormat extends Format[Array[RunLog]] {
-    def reads(json: JsValue): Array[RunLog] = {
-      val result = Array.ofDim[RunLog](json.asInstanceOf[JsArray].value.length)
-      var i = 0
-      json.asInstanceOf[JsArray].value.foreach { log =>
-        val logObj = log.as[JsObject]
-        val active = (logObj \ "active").asInstanceOf[JsArray]
-        val scaling = (logObj \ "scaling").asInstanceOf[JsArray]
-        val finalScaling = Array.ofDim[Float](active.value.length)
-        val activeLog = Array.ofDim[Boolean](active.value.length)
-        for (j <- 0 until finalScaling.length) {
-          //if (active(j).asInstanceOf[JsBoolean].value)
-          finalScaling(j) = scaling(j).as[Float]
-          activeLog(j) = active(j).as[Boolean]
-        }
-        result(i) = RunLog((logObj \ "name").as[String], finalScaling, activeLog)
-        i += 1
-      }
-      result
-    }
-    def writes(log: Array[RunLog]): JsValue = JsUndefined("TODO")
-  }
-
   def readLogs(OutDirectory: File): Option[Array[RunLog]] = {
-    val files = OutDirectory.listFiles(new FilenameFilter {
-      def accept(dir: File, name: String) = FilePattern.matcher(name).matches
-    })
-    var max = 0
-    var maxFile: File = null
-    for (file <- files) {
-      val matcher = FilePattern.matcher(file.getName)
-      if (matcher.matches) {
-        val num = matcher.group(1).toInt
-        if (num > max) {
-          max = num
-          maxFile = file
-        }
-      }
-    }
-    if (maxFile == null) None
-    else {
-      val in = new BufferedReader(new FileReader(maxFile))
-      val buffer = new StringBuilder
-      var line = in.readLine
-      while (line != null) {
-        buffer.append(line)
-        line = in.readLine
-      }
-      in.close
-      Some(play.api.libs.json.Json.parse(buffer.toString).as[Array[RunLog]])
-    }
+    val result = RunLogs.findAll.map { runLog =>
+      val entries = LogEntries.entriesFor(runLog._1).unzip
+      val active = entries._1.toArray
+      val scaling = entries._2.toArray
+      RunLog(runLog._2, scaling, active)
+    }.toArray
+    Some(result)
   }
 }
--- cluster_viewer/project/Build.scala
+++ cluster_viewer/project/Build.scala
@@ -9,6 +9,7 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+      "org.scalaquery" % "scalaquery_2.9.1" % "0.10.0-M1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
