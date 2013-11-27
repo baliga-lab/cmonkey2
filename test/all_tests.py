9cc286b843daca9c09f9db07ecc36c4e6805adfc
Wei-ju Wu <weiju.wu@gmail.com>
2013-11-27 09:58:53 -0800

    removed human_test, we will use a more flexible setup for user-defined runs in the future

--- test/all_tests.py
+++ test/all_tests.py
@@ -16,7 +16,6 @@ import microarray_test as mat
 import meme_test as met
 import pssm_test as pt
 import read_wee_test as rwt
-import human_test as ht
 import scoring_test as st
 
 
@@ -60,8 +59,6 @@ if __name__ == '__main__':
     SUITE.append(unittest.TestLoader().loadTestsFromTestCase(pt.PssmTest))
 
     SUITE.append(unittest.TestLoader().loadTestsFromTestCase(rwt.ReadWeeTest))
-
-    SUITE.append(unittest.TestLoader().loadTestsFromTestCase(ht.HumanTest))
 
     SUITE.append(unittest.TestLoader().loadTestsFromTestCase(st.DefaultScalingTest))
 
