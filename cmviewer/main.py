63a473f2b3caad8929bd7cccab5acb6a38231341
Wei-ju Wu <weiju.wu@gmail.com>
2014-02-27 14:05:06 -0800

    cluster table implemented

--- cmviewer/main.py
+++ cmviewer/main.py
@@ -19,14 +19,34 @@ RunInfo = namedtuple('RunInfo',
 ClusterStat = namedtuple('ClusterStat',
                          ['iter', 'cluster', 'num_rows', 'num_cols', 'residual'])
 
+IterationStat = namedtuple('IterationStat', ['iter', 'label', 'score'])
+
+MotifInfo = namedtuple('MotifInfo', ['id', 'cluster', 'seqtype', 'num', 'evalue'])
+
+MotifPSSMRow = namedtuple('MotifPSSMRow', ['motif_id', 'row', 'a', 'c', 'g', 't'])
+
+
 def runinfo_factory(cursor, row):
     return RunInfo(*row)
+
+def motifinfo_factory(cursor, row):
+    return MotifInfo(*row)
+
+def motifpssmrow_factory(cursor, row):
+    return MotifPSSMRow(*row)
+
 
 def clusterstat_factory(cursor, row):
     return ClusterStat(*row)
 
+
+def iterationstat_factory(cursor, row):
+    return IterationStat(*row)
+
+
 def dbconn():
     return sqlite3.connect(outdb)
+
 
 def make_int_histogram(counts):
     """input: list of counts
@@ -49,6 +69,14 @@ def make_float_histogram(values, nbuckets=20):
         yvals[bucketnum] += 1
     return json.dumps(xvals), json.dumps(yvals)
 
+
+def make_series(stats):
+    groups = defaultdict(list)
+    for stat in stats:
+        groups[stat.label].append(stat.score)
+    return json.dumps([{'name': label, 'data': groups[label]} for label in groups])
+
+
 class ClusterViewerApp:
 
     @cherrypy.expose
@@ -61,8 +89,10 @@ class ClusterViewerApp:
         cursor.close()
 
         cursor = conn.cursor()
-        cursor.execute('select median_residual from iteration_stats')
-        js_mean_residuals = json.dumps([row[0] for row in cursor.fetchall()])
+        cursor.execute('select median_residual, fuzzy_coeff from iteration_stats')
+        res = [(resid, fuzzy) for resid, fuzzy in cursor.fetchall()]
+        js_mean_residuals = json.dumps([row[0] for row in res])
+        js_fuzzy_coeff = json.dumps([row[1] for row in res])
         cursor.close()
 
         conn.row_factory = clusterstat_factory
@@ -90,6 +120,18 @@ class ClusterViewerApp:
         runinfo = cursor.fetchone()
         cursor.close()
 
+        conn.row_factory = iterationstat_factory
+        cursor = conn.cursor()
+        cursor.execute('select iteration, seqtype, pval from motif_stats')
+        js_motif_stats = make_series([row for row in cursor.fetchall()])
+        cursor.close()
+
+        cursor = conn.cursor()
+        cursor.execute('select iteration, network, score from network_stats')
+        js_network_stats = make_series([row for row in cursor.fetchall()])
+        cursor.close()
+
+
         conn.close()
         cursor= None
         conn = None
@@ -99,9 +141,77 @@ class ClusterViewerApp:
         current_iter = int(iteration)
         return tmpl.render(locals())
 
+    @cherrypy.expose
+    def clusters(self, iteration, *args, **kw):
+        conn = dbconn()
+        conn.row_factory = motifinfo_factory
+        cursor = conn.cursor()
+        cursor.execute("select rowid, cluster, seqtype, motif_num, evalue from motif_infos where iteration = ?", [iteration])
+        # grouped by cluster
+        motif_infos = defaultdict(list)
+        for row in cursor.fetchall():
+            motif_infos[row.cluster].append(row)
+        cursor.close()
+
+        conn.row_factory = motifpssmrow_factory
+        cursor = conn.cursor()
+        cursor.execute("select motif_info_id, row, a, c, g, t from motif_pssm_rows where iteration = ?", [iteration])
+        # grouped by motif info id
+        motif_pssm_rows = defaultdict(list)
+        for row in cursor.fetchall():
+            motif_pssm_rows[row.motif_id].append([row.a, row.c, row.g, row.t])
+        
+        cursor.close()
+
+        conn.row_factory = clusterstat_factory
+        cursor = conn.cursor()
+        cursor.execute("select iteration, cluster, num_rows, num_cols, residual from cluster_stats where iteration = ? order by residual", [iteration])
+        cluster_stats = [row for row in cursor.fetchall()]
+
+        rows = [["%d" % (i + 1),
+                 "<a class=\"clusterlink\" id=\"%d\"  href=\"#\">%d</a>" % (stat.cluster, stat.cluster),
+                 '%d' % stat.num_rows,
+                 '%d' % stat.num_cols,
+                 '%.2e' % stat.residual,            
+                 make_motif_string(motif_infos[stat.cluster],
+                                   motif_pssm_rows)]
+                for i, stat in enumerate(cluster_stats)]
+
+        cursor.close()
+        conn.close()
+        return json.dumps({'aaData': rows})
+
+
+def consensus(rows):
+    alphabet = ['A', 'C', 'G', 'T']
+    result = ''
+    for row in rows:
+        maxcol = 0
+        maxval = 0.0
+        for col in range(len(row)):
+            if row[col] > maxval:
+                maxcol = col
+                maxval = row[col]
+        result += alphabet[maxcol]
+    return result
+
+def make_motif_string(motif_infos, motif_pssm_rows):
+    result = "("
+    byseqtype = defaultdict(list)
+    for info in motif_infos:
+        byseqtype[info.seqtype].append(info)
+    for seqtype in byseqtype:
+        reps = ["%s (%.2e)" % (consensus(motif_pssm_rows[motif.id]), motif.evalue)
+                for motif in byseqtype[seqtype]]
+        result += "%s -> [%s]" % (seqtype, ', '.join(reps))
+    result += ")"
+    return result
+
 def setup_routes():
     d = cherrypy.dispatch.RoutesDispatcher()
-    d.connect('main', '/:iteration', controller=ClusterViewerApp(), action="iteration")
+    main = ClusterViewerApp()
+    d.connect('main', '/:iteration', controller=main, action="iteration")
+    d.connect('main', '/clusters/:iteration', controller=main, action="clusters")
     return d
 
 if __name__ == '__main__':
@@ -111,3 +221,7 @@ if __name__ == '__main__':
     cherrypy.config.update(conf)
     app = cherrypy.tree.mount(None, config=conf)
     cherrypy.quickstart(app)
+
+"""
+select cluster, residual, num_rows, num_cols from cluster_stats where iteration = 2000;
+"""
