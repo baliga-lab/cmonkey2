b3f02537655550d48a5e559bf82223fd66164502
Wei-ju Wu <weiju.wu@gmail.com>
2016-11-17 19:25:04 -0800

    changelog and version update for pip distribution

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = 'cmonkey2 is an implementation of the cmonkey biclustering method 
 LICENSE = 'LGPL V3'
 URI = 'https://github.com/baliga-lab/cmonkey2'
 AUTHOR = 'Baliga Lab, Institute for Systems Biology'
-VERSION = '1.2.2'
+VERSION = '1.2.3'
 
 KEYWORDS = ['class', 'cmonkey2']
 
